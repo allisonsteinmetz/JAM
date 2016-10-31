a2bca23df95548fab489a7220bd16a4c9ac69f3e
Josh Dickey <Juicearific@gmail.com>
2016-10-30 21:04:15 -0400

    Removed old implementation; made the output look nice
    Alli fixed the actual connection to GitHub, I removed the old framework
    as the way I was doing it is no longer necessary.
    
    I also modified the data that she was pulling down to display only our
    usernames.
    
    Next step is to hook it up to JS, hopefully.

--- test.py
+++ test.py
@@ -1,52 +1,23 @@
-GitHub_API = "https://api.github.com"
-
-import requests, time
 import json
 import os
-import Globalss
-from urlparse import urljoin
-from requests.auth import HTTPBasicAuth
 from agithub.GitHub import GitHub
 
 username = raw_input('Github username: ')
 password = raw_input('password: ')
-msg = ""
-#JAM_SECRET = 'test1'
-#secret = os.environ['JAM_SECRET']
-#id = os.environ['JAM_CLIENT_ID']
-
-def getAuthCode():
-    authorization_code_req = {
-        "response_typpe" : "code",
-        "client_id" : client_id,
-        "redirect_uri": redirect_uri,
-        "scope": ("https://www.google.com")
-    }
-
-    r = requests.get(base_url + "auth?%s" % urlencode(authorization_code_req),
-        allow_redirects=False)
 
 def main():
-    g = GitHub(username, password)
+    g = GitHub(username, password)  #use the inputted username and password to authenticate
     status, data = g.issues.get(filter='subscribed', foobar='llama')
-    print(status)
-    print(g.repos.allisonsteinmetz.JAM.collaborators.get())
-    # url = urljoin(GitHub_API, "authorizations")
-    # print(url)
-    # payload = {}
-    # res = requests.post (
-    #     "https://github.com/login/oauth/authorize?client_id=060bd7a32bdca5ebda07",
-    #     auth = HTTPBasicAuth(username, password),
-    #     data = json.dumps(payload),
-    # )
-    # print(res.text)
-    # j = json.loads(res.text)
-    # if res.status_code >= 400:
-    #     msg = j.get('message', 'UNDEFINED ERROR (no error description from server)')
-    #     print ('error: %s' %msg)
-    #     return
-    # token = j['token']
-    # print ('New token: %s' % token)
+    output = ''.join(map(str, g.repos.allisonsteinmetz.JAM.collaborators.get()))  #turn the tuple into a string
+    lookfor = "u'login'"    #this appears just before every username
+    nextspot = 0    #nextspot keeps track of the current spot we're finding. We don't want to find duplicates.
+    userlist = []
+    while(nextspot != -1):  #nextspot returns -1 if no more names were found.
+        nextspot = output.find(lookfor, nextspot+1)   #finds the lookfor phrase, starting after the current nextspot location
+        if (nextspot == -1):
+            break
+        tempstring = output[(nextspot+12):(nextspot+52)]   #removes all the jargon before a username, and limits it to the maximum amount of username chars (+1)
+        userlist.append(tempstring.partition("'")[0] )  #puts all the usernames into a list.
+    #print(userlist)
 
-#if __name__ == '__main__':
 main()
