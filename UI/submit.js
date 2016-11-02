3dbebb8a458edcbd3df8b20e92513515949091f3
Manan Patel <mananpatel.mp7@gmail.com>
2016-11-02 18:48:06 -0400

    Created ajax request to accept Python data
    Follow the links below to see how I am using ajax to accept data from
    Python:
    
    1)
    http://stackoverflow.com/questions/7689695/passing-variables-between-pyt
    hon-and-javascript
    
    2)
    http://stackoverflow.com/questions/31537946/passing-a-string-from-python
    -to-javascript
    
    —> used the second one

--- UI/submit.js
+++ UI/submit.js
@@ -1,38 +1,31 @@
-$(document).ready(function(){
-  var uName = document.getElementById("username").value;
-  var password = document.getElementById("pwd").value;
-    $("#button").click(function(){
-      $.ajax({
-        type: "POST",
-        url: "~/GitHubCommunicator.py",
-        data: {uName: password}
-      }).done(function( o ) {
-
-      });
-        window.open("success.html");
+$(document).ready(function () {
+    $("#button").click(function () {
+        var attempt = 3;
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("pwd").value;
+        if (username == "mvp" && password == "mvp123") {
+            console.log("Login Successfully!");
+            window.open("success.html"); // Redirecting to other page.
+        }
+        else {
+            attempt--; // Decrementing by one.
+            alert("You have " + attempt + " attempt left;");
+            // Disabling fields after 3 attempts.
+            if (attempt == 0) {
+                document.getElementById("username").disabled = true;
+                document.getElementById("pwd").disabled = true;
+                document.getElementById("button").disabled = true;
+            }
+        }
+        $.ajax({
+            url: "GitHubCommunicator.py"
+            ,type: "POST"
+            , success: function (response) {
+                console.log(JSON.stringify(response));
+            }
+            , error: function (xhr, errmsg, err) {
+                console.log("errmsg");
+            }
+        });
     });
 });
-
-
-var attempt = 2;
-
-function validate() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("pwd").value;
-    if (username == "Formget" && password == "formget#123") {
-        alert("Login successfully");
-        window.open = "success.html"; // Redirecting to other page.
-        return false;
-    }
-    else {
-        attempt--; // Decrementing by one.
-        alert("You have " + attempt + " attempt left;");
-        // Disabling fields after 3 attempts.
-        if (attempt == 0) {
-            document.getElementById("username").disabled = true;
-            document.getElementById("pwd").disabled = true;
-            document.getElementById("button").disabled = true;
-            return false;
-        }
-    }
-}
