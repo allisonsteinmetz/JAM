1f2fbd537b83629fdb2e477aee3cabc8db178621
allisonsteinmetz <ams5101@hotmail.com>
2016-11-11 12:28:26 -0500

    Fixed tests

--- test.py
+++ test.py
@@ -9,9 +9,9 @@ fake_pass = 'fake_password'
 real_user_list = ["allisonsteinmetz", "Juicearific", "MananVPatel"]
 
 def test_auth():
-    assert GitHubCommunicator.gAuth(real_user, real_pass) != False
-    assert GitHubCommunicator.gAuth(fake_user, fake_pass) == False
+    assert GitHubCommunicator.authenticate(real_user, real_pass) != False
+    assert GitHubCommunicator.authenticate(fake_user, fake_pass) == False
 
 def test_getUsers():
-    g = GitHubCommunicator.gAuth(real_user, real_pass)
+    g = GitHubCommunicator.authenticate(real_user, real_pass)
     assert GitHubCommunicator.getUsers(g) == real_user_list
