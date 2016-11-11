f0ec31fb409c0993ae332adf0fc9cdb6b7869cde
Josh Dickey <Juicearific@gmail.com>
2016-11-11 04:33:57 -0500

    I GOT IT WORKING GUYS
    As the summary says - python is finally displaying webpages. Potential
    references:
    
    http://stackoverflow.com/questions/10372883/simple-flask-application-that-reads-its-content-from-a-html-file-external-styl
    https://www.tutorialspoint.com/flask/flask_http_methods.htm
    
    So basically all the pages are python hosted and can be made in whatever
    we want, but we're kind of circumventing JS and just using Python for it
    instead. It's a nifty system, albeit it annoying to figure out the first
    time. Regardless, it works currently.
    
    It validates by GitHub, but GitHub will lock you out if you try
    incorrectly too many times. Incorrect tries reload the page, correct
    redirect you to the success page. We will probably want to remove the UI
    folder (since it basically just got split into templates and static
    anyways) - but I'm not worry about that right now.
    
    For future reference - all .html files should go in templates, all .css
    and .js should go in static.

--- GitHubCommunicator.py
+++ GitHubCommunicator.py
@@ -1,19 +1,42 @@
 import json
 import os
 from agithub.GitHub import GitHub
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for,  redirect, request
 from flask import make_response
 
 app = Flask(__name__)
 
-def gAuth(user, password):
-    g = GitHub(user, password)  #use the entered username and password to authenticate
-    status, data = g.issues.get()
-    if(status < 400):
-        return g
-    else:
-        return false
+#Authenticates a user based on username and password.
+#invalid credentials currently reloads the page.
+@app.route('/authenticate/')
+def authenticate(user, pwd):
+    g = GitHub(user, pwd)  #use the entered username and password to authenticate
+    status, data = g.issues.get() #get the status
+    if(status < 400):   #if the statue was not an error
+        return g    #return the token
+    else:   #if it was an error
+        return False    #return false
 
+#prints out the data on a success page
+@app.route('/success/<data>')
+def success(data):
+    return render_template('success.html', output=data) #calls the success.html page and feeds it the userlist as an argument
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST': #if submit button was pressed
+        username = request.form['username'] #read username
+        password = request.form['pwd']  #read password
+        token = authenticate(username, password)    #call our authentication
+        if (token == False):    #if the credentials were incorrect
+            return render_template('index.html')    #reload the page
+        else:   #if the credentials were correct
+            userlist = getUsers(token)  #get the users
+            return redirect(url_for('success', data = userlist))    #return the success page with our userlist
+    else:
+        return render_template('index.html')
+
+#gets a list of users from our predefined project.
 def getUsers(g):
     status, data = g.repos.allisonsteinmetz.JAM.collaborators.get()
     if status == 200:
@@ -24,21 +47,12 @@ def getUsers(g):
     else:
         return "Could not retrieve users"
 
-@app.route('/authenticate/<user>/<pw>')
-def main(user, pw): #main is basically the test case for this code
-    #request = json.load(sys.stdin)
-    #response = handle_request(request)
-    #print (json.dump(response, sys.stdout, indent=2))
+#just a homepage I was using for testing
+@app.route('/')
+def hello():
+    return 'Test Homepage'
 
-    # user = raw_input('Github username: ')
-    # pw = raw_input('password: ')
-    print("started")
-    g = gAuth(user, pw)
-    if (g):
-        userList = getUsers(g)
-        json_userList = json.dumps(userList)
-        print(json.dumps(json_userList, indent=2))
-        return json_userList
-
+#debug gives you information if a page fails to load.
+#port number is your choice - I had to keep changing it to avoid caching (I think?) errors.
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False, port = 4979)
