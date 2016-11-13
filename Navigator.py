173f462f62268101c01e126a9622345ab3885430
Allison Steinmetz <allison@easilydo.com>
2016-11-13 02:24:56 -0500

    More work on Navigator
    Mostly pseudocode, but I want to start getting the framework we were
    planning implemented. As such, I decided to put some work in on this
    navigator.

--- Navigator.py
+++ Navigator.py
@@ -12,3 +12,38 @@ def auth(user, pw):
         #return error message
     else:
         #display search page
+
+def showSearch():
+    #show search page
+
+def search(criteria, type):
+    if (type == "organizations"):
+        results = searchOrgs(criteria)
+    else:
+        results = searchProjects(criteria)
+    #display results
+
+def select(name, type):
+    if (type == "organizations"):
+        data = getOrgData(name)
+        analyzedData = analyzeOrg(data)
+    else :
+        data = getProjectData(name)
+        analyzedData = analyzeProject(data)
+    #store results to database
+    showUsers(analyzedData)
+
+def showUsers(info):
+    #show the user page
+
+def showUserInfo(name):
+    #show a specific user's statis
+
+def showTeam(number):
+    #show the team #(number)'s stats.
+
+def showLeadership(name):
+    #shows a specific user's leadership info
+
+def showContribution(name):
+    #shows a specific user's contribution info
