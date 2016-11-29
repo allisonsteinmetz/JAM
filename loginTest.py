315028f261b77ab15fa81e04ae1244f2be52f24c
allisonsteinmetz <ams5101@hotmail.com>
2016-11-29 10:43:41 -0500

    Selenium Test
    Added selenium test for login

---
+++ loginTest.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+real_user = 'juicearific'
+real_pass = 'demopw123'
+
+browser = webdriver.Chrome("/Users/allisonsteinmetz/Downloads/chromedriver")
+
+browser.get('http://localhost:4979/login/')
+
+username = browser.find_element_by_name('username')  # Find the username box
+username.send_keys(real_user)
+password = browser.find_element_by_name('pwd') # Find the password box
+password.send_keys(real_pass + Keys.RETURN)
+
+browser.quit()
