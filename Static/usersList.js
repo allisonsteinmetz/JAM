d841c26fcdc1697ecd335dcb0aec8e9a0f8b0f37
allisonsteinmetz <ams5101@hotmail.com>
2017-03-29 15:42:59 -0400

    Updated percentages and added lines of code chart

--- Static/usersList.js
+++ Static/usersList.js
@@ -3,10 +3,6 @@ function loadHome(){
 }
 function fixFormatting()
 {
-  scores = document.getElementsByClassName("contributionScore");
-  for (i = 0; i < scores.length; i++){
-    scores[i].innerHTML = (((scores[i].innerHTML) * 100).toFixed(2)) + '%';
-  }
   languages = document.getElementsByClassName("languages");
   for (i = 0; i < languages.length; i++){
     userLangs = languages[i];
